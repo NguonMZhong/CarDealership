da5cc50bc8baf69db1684329ec499f588ae550ad
Nguon.Z <nzhong@appdev.yearup.org>
2023-05-12 08:47:00 -0500

    new project

---
+++ src/main/java/org/yearup/Dealership.java
@@ -0,0 +1,5 @@
+package org.yearup;
+
+public class Dealership
+{
+}
---
+++ src/main/java/org/yearup/Program.java
@@ -0,0 +1,9 @@
+package org.yearup;
+
+public class Program
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
